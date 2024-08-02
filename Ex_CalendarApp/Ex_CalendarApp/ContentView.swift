14e606f213a881305883edc6867331c6f13f3eca
Saikaku-69 <grnmnsta@gmail.com>
2024-08-02 23:16:44 +0900

    8/2 Start
    ToList練習

---
+++ Ex_CalendarApp/Ex_CalendarApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ex_CalendarApp
+//
+//  Created by cmStudent on 2024/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ex_CalendarApp/Ex_CalendarApp/Ex_CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex_CalendarAppApp.swift
+//  Ex_CalendarApp
+//
+//  Created by cmStudent on 2024/08/02.
+//
+
+import SwiftUI
+
+@main
+struct Ex_CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
